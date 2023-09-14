850af75d60e1d6b82fbb1da9527582c65a99fc1a
dre <dwaynea.mclaren@gmail.com>
2023-09-14 14:49:52 -0500

    Add project files.

---
+++ Chargie/chargie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Chargie
+{
+    public class chargie
+    {
+
+        public string name;
+        public char gender;
+        public int age;
+
+
+
+        public chargie() {
+            name = "andre";
+        
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            chargie friend = new chargie();
+
+            Console.WriteLine(friend.name);
+        }
+    }
+
+
+
+
+
+    
+}
